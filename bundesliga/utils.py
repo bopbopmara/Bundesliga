ba0d5550bb92706c6576fe68a3d23dc998687828
Maria Nikolova <mara.ovni@gmail.com>
2017-04-04 12:43:17 +0300

    A utility for retrieving upcoming matches

---
+++ bundesliga/utils.py
@@ -0,0 +1,50 @@
+import requests
+from datetime import date
+
+matchday_url = lambda league: 'https://www.openligadb.de/api/getcurrentgroup/{}'.format(league)
+
+matches_for_matchday_url = lambda league, year, matchday: 'https://www.openligadb.de/api/getmatchdata/{league}/{year}/{matchday}'.format(
+    league=league,
+    year=year,
+    matchday=matchday,
+)
+
+
+def retrieve_upcoming_matches(league):
+    # Retrieve data for current matchday
+    response = requests.get(matchday_url(league))
+    if response.status_code != 200:
+        return []
+
+    matchday = response.json()['GroupOrderID']
+
+    # Try to retrieve matches for current matchday
+    # for the tournament edition that started this year
+    year = date.today().year
+    response = requests.get(matches_for_matchday_url(league, year, matchday))
+    if response.status_code != 200:
+        return []
+
+    matches_data = response.json()
+
+    # If no matches are found we're probably at the second
+    # half of the tournament edition that started last year
+    if not matches_data:
+        year -= 1
+        response = requests.get(matches_for_matchday_url(league, year, matchday))
+        if response.status_code != 200:
+            return []
+
+        matches_data = response.json()
+
+    # If all matches for the current matchday have already ended
+    # we actually need to retrieve matches for the next matchday
+    if matches_data and all([match_data['MatchIsFinished'] for match_data in matches_data]):
+        matchday += 1
+        response = requests.get(matches_for_matchday_url(league, year, matchday))
+        if response.status_code != 200:
+            return []
+
+        matches_data = response.json()
+
+    return matches_data
---
+++ tests/test_utils.py
@@ -0,0 +1,211 @@
+from unittest.mock import patch
+
+from freezegun import freeze_time
+
+from bundesliga.utils import (
+    retrieve_upcoming_matches,
+)
+
+
+class APIResponse(object):
+    def __init__(self, status_code=200, content=None):
+        self.status_code = status_code
+        self.content = content
+
+    def json(self):
+        return self.content
+
+
+@freeze_time('2017-06-01')
+def test_retrieve_upcoming_matches():
+    # Current matchday retrieval fails
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == []
+
+    # The upcoming matches are for the current matchday of the tournament edition that started this year
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == []
+
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                    'MatchIsFinished': False,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-06-26T20:00:00Z',
+                    'MatchIsFinished': True,
+                },
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == [
+            {
+                'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                'MatchIsFinished': False,
+            },
+            {
+                'MatchDateTimeUTC': '2017-06-26T20:00:00Z',
+                'MatchIsFinished': True,
+            },
+        ]
+
+    # The upcoming matches are for the next matchday of the tournament edition that started this year
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-05-26T18:30:00Z',
+                    'MatchIsFinished': True,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-05-26T20:00:00Z',
+                    'MatchIsFinished': True,
+                },
+            ])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/3':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == []
+
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-05-26T18:30:00Z',
+                    'MatchIsFinished': True,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-05-26T20:00:00Z',
+                    'MatchIsFinished': True,
+                },
+            ])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/3':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                    'MatchIsFinished': False,
+                },
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == [
+            {
+                'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                'MatchIsFinished': False,
+            },
+        ]
+
+    # The upcoming matches are for the current matchday of the tournament edition that started last year
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/2':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == []
+
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                    'MatchIsFinished': True,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-06-26T20:00:00Z',
+                    'MatchIsFinished': False,
+                },
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == [
+            {
+                'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                'MatchIsFinished': True,
+            },
+            {
+                'MatchDateTimeUTC': '2017-06-26T20:00:00Z',
+                'MatchIsFinished': False,
+            },
+        ]
+
+    # The upcoming matches are for the next matchday of the tournament edition that started last year
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-05-26T18:30:00Z',
+                    'MatchIsFinished': True,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-05-26T20:00:00Z',
+                    'MatchIsFinished': True,
+                },
+            ])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/3':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == []
+
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getcurrentgroup/bl1':
+            return APIResponse(content={'GroupOrderID': 2})
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017/2':
+            return APIResponse(content=[])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/2':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-05-26T18:30:00Z',
+                    'MatchIsFinished': True,
+                },
+                {
+                    'MatchDateTimeUTC': '2017-05-26T20:00:00Z',
+                    'MatchIsFinished': True,
+                },
+            ])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016/3':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                    'MatchIsFinished': False,
+                },
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_upcoming_matches('bl1') == [
+            {
+                'MatchDateTimeUTC': '2017-06-26T18:30:00Z',
+                'MatchIsFinished': False,
+            },
+        ]
