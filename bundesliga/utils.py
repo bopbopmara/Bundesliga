8461ada659505178bbf98851d501992aa39bc934
Maria Nikolova <mara.ovni@gmail.com>
2017-04-04 14:03:47 +0300

    A utility for building league statistics

--- bundesliga/utils.py
+++ bundesliga/utils.py
@@ -1,6 +1,10 @@
 import requests
 from datetime import date
 
+POINTS_WIN = 3
+
+POINTS_DRAW = 1
+
 matchday_url = lambda league: 'https://www.openligadb.de/api/getcurrentgroup/{}'.format(league)
 
 matches_for_league_url = lambda league, year: 'https://www.openligadb.de/api/getmatchdata/{league}/{year}'.format(
@@ -12,6 +16,11 @@ matches_for_matchday_url = lambda league, year, matchday: 'https://www.openligad
     league=league,
     year=year,
     matchday=matchday,
+)
+
+teams_for_league_url = lambda league, year: 'https://www.openligadb.de/api/getavailableteams/{league}/{year}'.format(
+    league=league,
+    year=year,
 )
 
 
@@ -75,3 +84,54 @@ def retrieve_all_matches(league):
         matches_data = response.json()
 
     return matches_data, year
+
+
+def retrieve_all_teams(league, year):
+    response = requests.get(teams_for_league_url(league, year))
+    if response.status_code != 200:
+        return []
+
+    return response.json()
+
+
+def build_statistics(league):
+    matches_data, year = retrieve_all_matches(league)
+
+    statistics = {
+        team['TeamName']: {'wins': 0, 'draws': 0, 'losses': 0, 'points': 0, 'difference': 0}
+        for team in retrieve_all_teams(league, year)
+    }
+
+    for match_data in matches_data:
+        if not match_data['MatchIsFinished']:
+            break
+
+        host_name = match_data['Team1']['TeamName']
+        guest_name = match_data['Team2']['TeamName']
+        host_score, guest_score = None, None
+
+        for result in match_data['MatchResults']:
+            if result['ResultTypeID'] == 2:
+                host_score = result['PointsTeam1']
+                guest_score = result['PointsTeam2']
+                break
+
+        if host_score > guest_score:
+            statistics[host_name]['wins'] += 1
+            statistics[host_name]['points'] += POINTS_WIN
+            statistics[host_name]['difference'] += (host_score - guest_score)
+            statistics[guest_name]['losses'] += 1
+            statistics[guest_name]['difference'] += (guest_score - host_score)
+        elif host_score < guest_score:
+            statistics[guest_name]['wins'] += 1
+            statistics[guest_name]['points'] += POINTS_WIN
+            statistics[guest_name]['difference'] += (guest_score - host_score)
+            statistics[host_name]['losses'] += 1
+            statistics[host_name]['difference'] += (host_score - guest_score)
+        else:
+            statistics[host_name]['draws'] += 1
+            statistics[host_name]['points'] += POINTS_DRAW
+            statistics[guest_name]['draws'] += 1
+            statistics[guest_name]['points'] += POINTS_DRAW
+
+    return statistics
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,9 @@ from unittest.mock import patch
 from freezegun import freeze_time
 
 from bundesliga.utils import (
+    build_statistics,
     retrieve_all_matches,
+    retrieve_all_teams,
     retrieve_upcoming_matches,
 )
 
@@ -283,3 +285,134 @@ def test_retrieve_all_matches():
                 'MatchIsFinished': False,
             },
         ], 2016)
+
+
+def test_retrieve_teams():
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getavailableteams/bl1/2016':
+            return APIResponse(status_code=500)
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_all_teams('bl1', 2016) == []
+
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getavailableteams/bl1/2016':
+            return APIResponse(content=[
+                {'TeamName': 'Borussia Dortmund'},
+                {'TeamName': 'Bayern München'},
+                {'TeamName': 'RB Leipzig'},
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        assert retrieve_all_teams('bl1', 2016) == [
+            {'TeamName': 'Borussia Dortmund'},
+            {'TeamName': 'Bayern München'},
+            {'TeamName': 'RB Leipzig'},
+        ]
+
+
+@freeze_time('2017-06-01')
+def test_build_statistics():
+    def requests_get(url):
+        if url == 'https://www.openligadb.de/api/getavailableteams/bl1/2016':
+            return APIResponse(content=[
+                {'TeamName': 'Borussia Dortmund'},
+                {'TeamName': 'Bayern München'},
+                {'TeamName': 'RB Leipzig'},
+                {'TeamName': 'SV Darmstadt 98'},
+            ])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2017':
+            return APIResponse(content=[])
+        if url == 'https://www.openligadb.de/api/getmatchdata/bl1/2016':
+            return APIResponse(content=[
+                {
+                    'MatchDateTimeUTC': '2016-09-10T16:30:00Z',
+                    'Team1': {'TeamName': 'RB Leipzig', 'TeamIconUrl': 'http://www.sportal.de/photos/fussball/logos/35x35/1583.png'},
+                    'Team2': {'TeamName': 'Borussia Dortmund', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif'},
+                    'MatchIsFinished': True,
+                    'MatchResults': [
+                        {'PointsTeam1': 0, 'PointsTeam2': 0, 'ResultOrderID': 1, 'ResultTypeID': 1},
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 2, 'ResultTypeID': 2},
+                    ],
+                },
+                {
+                    'MatchDateTimeUTC': '2016-11-19T17:30:00Z',
+                    'Team1': {'TeamName': 'Borussia Dortmund', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif'},
+                    'Team2': {'TeamName': 'Bayern München', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Bayern_Muenchen.gif'},
+                    'MatchIsFinished': True,
+                    'MatchResults': [
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 1, 'ResultTypeID': 1},
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 2, 'ResultTypeID': 2},
+                    ],
+                },
+                {
+                    'MatchDateTimeUTC': '2016-12-21T19:00:00Z',
+                    'Team1': {'TeamName': 'Bayern München', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Bayern_Muenchen.gif'},
+                    'Team2': {'TeamName': 'RB Leipzig', 'TeamIconUrl': 'http://www.sportal.de/photos/fussball/logos/35x35/1583.png'},
+                    'MatchIsFinished': True,
+                    'MatchResults': [
+                        {'PointsTeam1': 3, 'PointsTeam2': 0, 'ResultOrderID': 1, 'ResultTypeID': 1},
+                        {'PointsTeam1': 3, 'PointsTeam2': 0, 'ResultOrderID': 2, 'ResultTypeID': 2},
+                    ],
+                },
+                {
+                    'MatchDateTimeUTC': '2017-02-04T17:30:00Z',
+                    'Team1': {'TeamName': 'Borussia Dortmund', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif'},
+                    'Team2': {'TeamName': 'RB Leipzig', 'TeamIconUrl': 'http://www.sportal.de/photos/fussball/logos/35x35/1583.png'},
+                    'MatchIsFinished': True,
+                    'MatchResults': [
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 1, 'ResultTypeID': 1},
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 2, 'ResultTypeID': 2},
+                    ],
+                },
+                {
+                    'MatchDateTimeUTC': '2017-04-08T16:30:00Z',
+                    'Team1': {'TeamName': 'Bayern München', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Bayern_Muenchen.gif'},
+                    'Team2': {'TeamName': 'Borussia Dortmund', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif'},
+                    'MatchIsFinished': True,
+                    'MatchResults': [
+                        {'PointsTeam1': 1, 'PointsTeam2': 0, 'ResultOrderID': 1, 'ResultTypeID': 1},
+                        {'PointsTeam1': 1, 'PointsTeam2': 1, 'ResultOrderID': 2, 'ResultTypeID': 2},
+                    ],
+                },
+                {
+                    'MatchDateTimeUTC': '2017-05-13T13:30:00Z',
+                    'Team1': {'TeamName': 'RB Leipzig', 'TeamIconUrl': 'http://www.sportal.de/photos/fussball/logos/35x35/1583.png'},
+                    'Team2': {'TeamName': 'Bayern München', 'TeamIconUrl': 'http://www.openligadb.de/images/teamicons/Bayern_Muenchen.gif'},
+                    'MatchIsFinished': False,
+                    'MatchResults': [],
+                },
+            ])
+
+    with patch('bundesliga.utils.requests.get', side_effect=requests_get):
+        statistics = build_statistics('bl1')
+
+    assert len(statistics.keys()) == 4
+    assert statistics['Borussia Dortmund'] == {
+        'wins': 2,
+        'draws': 1,
+        'losses': 1,
+        'points': 7,
+        'difference': 1,
+    }
+    assert statistics['Bayern München'] == {
+        'wins': 1,
+        'draws': 1,
+        'losses': 1,
+        'points': 4,
+        'difference': 2,
+    }
+    assert statistics['RB Leipzig'] == {
+        'wins': 1,
+        'draws': 0,
+        'losses': 2,
+        'points': 3,
+        'difference': -3,
+    }
+    assert statistics['SV Darmstadt 98'] == {
+        'wins': 0,
+        'draws': 0,
+        'losses': 0,
+        'points': 0,
+        'difference': 0,
+    }
